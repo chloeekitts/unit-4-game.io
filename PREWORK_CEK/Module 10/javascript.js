8d67b29e6741a20d4e5396e83d7fd7563a7c2c42
Chloe Kitts <chloeekitts@gmail.com>
2019-03-16 10:33:00 -0500

    commiting classwork stuff 3-16-19

---
+++ PREWORK_CEK/Module 10/javascript.js
@@ -0,0 +1,27 @@
+<script type="text/javascript">
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "25px"
+
+});
+
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px"
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px"
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+document.getElementById("box").style.height = "150px"
+
+});
+
+</script>
